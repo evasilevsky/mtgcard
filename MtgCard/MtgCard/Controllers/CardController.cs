8afae1279349f77eadb5669f22fffbed3c3edbae
Eric Vasilevsky <evasilevsky@gmail.com>
2015-01-08 09:58:00 -0700

    Removing unused constructor.

--- MtgCard/MtgCard/Controllers/CardController.cs
+++ MtgCard/MtgCard/Controllers/CardController.cs
@@ -7,12 +7,7 @@ namespace MtgCard.Controllers
     public class CardController : ApiController
     {
 	    private CardAdapter _cardAdapter = new CardAdapter();
-
-	    public CardController()
-	    {
-		    
-	    }
-
+		
         public Card Get(string cardName)
         {
             return _cardAdapter.GetCardByName(cardName);
