e466d754d7d2d2966a0940b703a5bd006ae4d1b4
Eric Vasilevsky <evasilevsky@gmail.com>
2015-01-07 18:01:52 -0700

    New MVC5 Project Check In

---
+++ MtgCard/MtgCard/Controllers/ErrorController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace MtgCard.Controllers
+{
+	public class ErrorController : Controller
+	{
+		public ActionResult Http404()
+		{
+			Response.StatusCode = 404;
+
+			return View();
+		}
+	}
+}
