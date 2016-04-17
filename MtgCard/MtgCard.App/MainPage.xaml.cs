d771efcd1a9af34e9eb23f9367bcccd4b1f3eb10
Eric Vasilevsky <evasilevsky@gmail.com>
2016-04-17 16:29:04 -0600

    Added image

--- MtgCard/MtgCard.App/MainPage.xaml.cs
+++ MtgCard/MtgCard.App/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -23,8 +24,8 @@ namespace MtgCard.App
     public sealed partial class MainPage : Page
     {
         public MainPage()
-        {
-            this.InitializeComponent();
+		{
+			this.InitializeComponent();
         }
     }
 }
