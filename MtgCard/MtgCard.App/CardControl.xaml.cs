ac7334c20dfbfcc8b552df4b52ca3355c2768cf8
Eric Vasilevsky <evasilevsky@gmail.com>
2016-04-19 03:09:16 -0600

    Can now add/remove copies of a card.

--- MtgCard/MtgCard.App/CardControl.xaml.cs
+++ MtgCard/MtgCard.App/CardControl.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
@@ -28,12 +29,20 @@ namespace MtgCard.App
 			this.InitializeComponent();
 			card = new Card();
 			card.copies = 2;
+			card.editions = new List<Edition> { new Edition { image_Url= "https://image.deckbrew.com/mtg/multiverseid/382943.jpg" } };
+			image.Source = new BitmapImage(new Uri(card.DefaultImage, UriKind.Absolute));
 			this.DataContext = this;
 		}
 
 		private void addCopy_Click(object sender, RoutedEventArgs e)
 		{
+			if (!removeCopy.IsEnabled)
+			{
+				removeCopy.IsEnabled = true;
+				image.Opacity = 1;
+			}
 			card.copies++;
+			copies.Text = card.copies.ToString();
 		}
 
 		private void removeCopy_Click(object sender, RoutedEventArgs e)
@@ -41,8 +50,14 @@ namespace MtgCard.App
 			card.copies--;
 			if (card.copies == 0)
 			{
-				Delete?.Invoke();
+				image.Opacity = .2;
+				removeCopy.IsEnabled = false;
 			}
+			else
+			{
+				image.Opacity = 1;
+			}
+			copies.Text = card.copies.ToString();
 		}
 	}
 }
